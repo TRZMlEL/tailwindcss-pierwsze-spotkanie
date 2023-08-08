565c2aef4a804ee0e54691357432f246af84159b
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-08-08 20:43:01 +0200

    new file:   .gitignore 	new file:   dist/output.css 	new file:   package-lock.json 	new file:   package.json 	new file:   src/index.html 	new file:   src/input.css 	new file:   tailwind.config.js 	new file:   themes.txt

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+        'text': '#e4f6e8',
+        'background': '#010402',
+        'primary': '#1d5429',
+        'secondary': '#12351a',
+        'accent': '#50c36b',
+      },
+    extend: {},
+  },
+  plugins: [],
+}
